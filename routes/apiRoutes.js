5f864daf42b867b02c5de36726ae65e0f464fdde
unknown <Shellynguyen98@gmail.com>
2020-08-19 23:21:46 -0700

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,22 +14,25 @@ router.get('/api/notes', (req, res) => {
 
 // POST
 router.post('/api/notes', (req, res) => {
-    
+
     fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) { console.log(err) }
+  
+      let notes = JSON.parse(data)
+      let note = {
+        id: uuid.v1(),
+        title: req.body.title,
+        text: req.body.text
+      }
+      notes.push(note)
+  
+      fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
         if (err) { console.log(err) }
-        let notes = JSON.parse(data)
-        let note = {
-            id: uuid.v1(),
-            title: req.body.title,
-            text: req.body.text,
-        }
-        notes.push(note)
-        fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
-            if (err) { console.log(err) }
-        })
+  
         res.json(note)
+      })
     })
-})
+  })
 
 // DELETE
 router.delete('/api/notes/:id', (req, res) => {
